cbead4fd33e3979822fbb4b80f8e5ef4261576f8
Alex <alexnezhynsky@gmail.com>
2019-06-19 21:44:36 -0400

    chore: Set up a dev server and HTML template

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const canvas = document.getElementById('canvas') as HTMLCanvasElement
+
+const context = canvas.getContext('2d') as CanvasRenderingContext2D
+
+context.fillStyle = '#000'
+
+context.fillRect(0, 0, 30, 30)
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,24 @@
-module.exports = {
-  resolve: {
-    extensions: ['.ts']
-  },
-  module: {
-    rules: [{ test: /\.ts$/, loader: 'ts-loader', exclude: /node_modules/ }]
+/* eslint-disable @typescript-eslint/no-var-requires */
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = (env, { mode }) => {
+  const inDev = mode === 'development'
+
+  return {
+    devtool: inDev ? 'cheap-module-eval-source-map' : 'source-map',
+    output: {
+      filename: inDev ? '[name].js' : '[name].[contenthash].js'
+    },
+    resolve: {
+      extensions: ['.ts', '.js']
+    },
+    module: {
+      rules: [{ test: /\.ts$/, loader: 'ts-loader', exclude: /node_modules/ }]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'index.html'
+      })
+    ]
   }
 }
